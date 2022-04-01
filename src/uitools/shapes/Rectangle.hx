becf81e9dde2b6a97bd0082ca97effb24e300736
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-04-01 12:56:34 -0400

    Update Rectangle.hx

--- src/uitools/shapes/Rectangle.hx
+++ src/uitools/shapes/Rectangle.hx
@@ -3,5 +3,7 @@ package uitools.shapes;
 import flixel.FlxObject;
 
 class Rectangle extends FlxObject {
-
+  public function new(?width:Int = 100, ?height:Int = 100, ?color:FlxColor = FlxColor.WHITE, ?x:Int = 0, ?y:Int = 0) {
+  
+  }
 }
